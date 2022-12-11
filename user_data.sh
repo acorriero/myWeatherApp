3c6edff7a854281247d99736bef096cdf0dc0464
acorriero <anthony.corriero@gmail.com>
2022-12-10 20:37:02 -0500

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo apt-get update -y
-sudo apt-get install apache2 -y
-sudo systemctl start apache2
-sudo systemctl enable apache2
-echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+sudo apt-get install docker.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
