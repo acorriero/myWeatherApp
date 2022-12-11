c6ad15618947c496f7749434ab339f6d416339f6
Anthony Corriero <anthony.corriero@gmail.com>
2022-12-11 09:54:06 -0500

    added Dockerfile and setup to run nginx on ec2 instance

--- user_data.sh
+++ user_data.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-get install docker.io -y
-sudo systemctl start docker
-sudo systemctl enable docker
+apt-get update -y
+apt-get install docker.io -y
+systemctl start docker
+systemctl enable docker
+
+cd /tmp
+cat > Dockerfile << EOF
+FROM nginx:1.23
+EOF
+
+docker build -t weather:0.1 .
+docker run -d --name weather -p 80:80 --restart always weather:0.1
