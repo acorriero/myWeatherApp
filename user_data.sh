b47374de03224fea09189929c96f376e153d82e4
Anthony Corriero <anthony.corriero@gmail.com>
2022-12-27 09:59:36 -0500

    update to install aws version 2

--- user_data.sh
+++ user_data.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 apt-get update -y
-apt-get install docker.io awscli -y
+apt-get install docker.io -y
 systemctl start docker
 systemctl enable docker
+
+cd /tmp
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 
 docker login -u AWS -p $(aws ecr get-login-password --region us-east-1) 503680398283.dkr.ecr.us-east-1.amazonaws.com
 
