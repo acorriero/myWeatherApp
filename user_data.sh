aa02be93f5949adb1c691498d2caaceb74c2c5fe
Anthony Corriero <anthony.corriero@gmail.com>
2022-12-27 10:34:02 -0500

    correction made to user_data for pulling ecr image

--- user_data.sh
+++ user_data.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-apt-get update -y
-apt-get install docker.io unzip -y
+apt-get update
+apt-get install docker.io -y
 systemctl start docker
 systemctl enable docker
 
-cd /tmp
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
+aws ecr get-login-password --region ${var.region} | docker login --username AWS --password-stdin ${aws_ecr_repository.my_weather_app.repository_url}
 
-docker login -u AWS -p $(aws ecr get-login-password --region us-east-1) 503680398283.dkr.ecr.us-east-1.amazonaws.com
+docker pull ${aws_ecr_repository.my_weather_app.repository_url}:latest
 
-docker pull 503680398283.dkr.ecr.us-east-1.amazonaws.com/my_weather_app:latest
-
-docker run -d --name my_weather_app -p 80:8501 503680398283.dkr.ecr.us-east-1.amazonaws.com/my_weather_app
+docker run -d --name my_weather_app -p 80:8501 ${aws_ecr_repository.my_weather_app.repository_url}:latest
