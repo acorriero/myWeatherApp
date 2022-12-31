193a1a6b1cd7c9ea8104cdebb3e7380ae8f4eefa
acorriero <anthony.corriero@gmail.com>
2022-12-31 15:39:13 -0500

    Update main.tf
    remote-exec requires a connection string. will add in separate file

--- main.tf
+++ main.tf
@@ -135,17 +135,6 @@ resource "aws_instance" "web-server" {
     ]
   }
 
-  provisioner "remote-exec" {
-    inline = [
-      "apt-get update",
-      "apt-get install docker.io awscli -y",
-      "systemctl start docker",
-      "systemctl enable docker",
-      "aws ecr get-login-password --region ${var.region} | docker login --username AWS --password-stdin ${var.ecr_rep}",
-      "docker pull ${var.ecr_rep}:latest",
-      "docker run -d --name my_weather_app -p 80:8501 ${var.ecr_rep}:latest"
-    ]
-  }
   # user_data = data.template_file.user_data.rendered
 
   tags = {
